57fe8940135d285ff3641f2bfba057356ad1ce57
Enguerran <enguerran@ticabri.com>
2017-02-11 20:18:55 +0100

    style: add material-ui :nail_care:

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react'
 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme'
+
+import RaisedButton from 'material-ui/RaisedButton'
+import AppBar from 'material-ui/AppBar'
+
 export default () => (
-  <div>
-    <h1>Hello React</h1>
-    <p>React Hot Loader</p>
-  </div>
+  <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+    <div>
+      <AppBar title='My AppBar' />
+      <h1>Hello React</h1>
+      <p>React Hot Loader</p>
+      <RaisedButton label='Default' />
+    </div>
+  </MuiThemeProvider>
 )
